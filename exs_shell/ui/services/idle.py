0eed94b8b88c6ebe5e2e7856f85e3b0047479d9b
kipoha <busterx25@gmail.com>
2026-03-01 18:12:06 +0600

    add custom idle getter

--- exs_shell/ui/services/idle.py
+++ exs_shell/ui/services/idle.py
@@ -9,6 +9,7 @@ from ignis.utils import exec_sh_async
 from exs_shell import register
 from exs_shell.configs.user import user
 from exs_shell.utils.loop import run_async_task
+from exs_shell.utils.path import Paths
 
 
 @register.event
@@ -18,53 +19,73 @@ class IdleService(BaseService):
         self.actions = user.get_idle_actions_objs()
         self._executed = [False] * len(self.actions)
         self._is_idle = False
-        self._last_event = time.time()
-        self._timer_interval = 1
+        self._idle_start = None
 
         self._proc = None
-        self._listen_event_stream()
-        GLib.timeout_add_seconds(self._timer_interval, self._check_idle)
+        self._start_idle_binary()
 
-    def _listen_event_stream(self):
+        GLib.timeout_add_seconds(1, self._check_idle)
+
+    def _start_idle_binary(self):
+        idle_binary = str(Paths.path / "extensions/idle/wl_idle_helper")
         self._proc = subprocess.Popen(
-            ["niri", "msg", "event-stream"],
+            [idle_binary, "2000"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
+            bufsize=1,
         )
 
-        GLib.io_add_watch(self._proc.stdout.fileno(), GLib.IO_IN, self._on_event_line)  # type: ignore
+        GLib.io_add_watch(self._proc.stdout.fileno(), GLib.IO_IN, self._on_idle_stdout)  # type: ignore
+        GLib.io_add_watch(self._proc.stderr.fileno(), GLib.IO_IN, self._on_idle_stderr)  # type: ignore
 
     @register.events.option(user, "idle_actions")
     def __update(self, *_):
         self.actions = user.get_idle_actions_objs()
         self._executed = [False] * len(self.actions)
 
-    def _on_event_line(self, fd, condition):
+    def _on_idle_stdout(self, fd, condition):
         line = self._proc.stdout.readline()  # type: ignore
-        if line.strip():
-            self._last_event = time.time()
+        if not line:
+            return True
+
+        line = line.strip()
+        if not line:
+            return True
+
+        if line == "idle":
+            if not self._is_idle:
+                self._is_idle = True
+                self._idle_start = time.time()
+        elif line == "resume":
+            if self._is_idle:
+                self._is_idle = False
+                self._idle_start = None
+                for i, action in enumerate(self.actions):
+                    if self._executed[i] and action.on_resume:
+                        run_async_task(exec_sh_async(action.on_resume))
+                self._executed = [False] * len(self.actions)
+        else:
+            self._is_idle = False
+            self._idle_start = None
+            self._executed = [False] * len(self.actions)
+
+        return True
+
+    def _on_idle_stderr(self, fd, condition):
+        line = self._proc.stderr.readline()  # type: ignore
+        if line:
+            print("wl_idle_helper stderr:", line.strip())
         return True
 
     def _check_idle(self):
-        now = time.time()
-        idle_seconds = now - self._last_event
-
-        if idle_seconds < 1:
-            if self._is_idle:
-                for i, action in enumerate(self.actions):
-                    if self._executed[i]:
-                        if action.on_resume:
-                            run_async_task(exec_sh_async(action.on_resume))
-                self._is_idle = False
-                self._executed = [False] * len(self.actions)
-        else:
-            if not self._is_idle:
-                self._is_idle = True
-
+        if self._is_idle and self._idle_start is not None:
+            print("IDLE:", self._is_idle)
+            print("IDLE seconds:", time.time() - self._idle_start)
+            now = time.time()
+            idle_seconds = now - self._idle_start
             for i, action in enumerate(self.actions):
                 if not self._executed[i] and idle_seconds >= action.timeout_seconds:
                     run_async_task(exec_sh_async(action.on_timeout))
                     self._executed[i] = True
-
         return True
